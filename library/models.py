b1085cd9ae2fa41b785d550da4192315dc567598
Ivan <maramorosh2@gmail.com>
2023-05-05 09:41:50 +0300

    Add serializers & views & urls for library app

--- library/models.py
+++ library/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    book_cover_choices = [
+        ("HARD", "HARD"),
+        ("SOFT", "SOFT"),
+    ]
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=4,
+        choices=book_cover_choices,
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(decimal_places=2, max_digits=4)
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return = models.DateField()
+    actual_return = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- library/views.py
+++ library/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from library.models import Book, Borrowing
+from library.serializers import BookSerializer, BorrowingSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
